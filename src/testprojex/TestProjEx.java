5061ac1aa9073079235ac6d903026486ea8c2b38
SSmorez <fgwkly2@gmail.com>
2021-12-01 08:41:23 -0800

    Hello to hi
    Changed Hello to hi

--- src/testprojex/TestProjEx.java
+++ src/testprojex/TestProjEx.java
@@ -15,7 +15,7 @@ public class TestProjEx {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("hello");
+        System.out.println("hi");
         //dee snuts
     }
     

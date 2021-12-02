a33a7b7051c26ea288b63f1d4a9106eeb8bbbffa
Jared <jaredwombat@gmail.com>
2021-12-02 08:51:24 -0800

    342
    342

--- src/testprojex/TestProjEx.java
+++ src/testprojex/TestProjEx.java
@@ -17,8 +17,6 @@ public class TestProjEx {
     public static void main(String[] args) {
         System.out.println("hi");
         //hi
-        System.out.println("hi");
-        System.out.println("hi");
         ///
     }
     
